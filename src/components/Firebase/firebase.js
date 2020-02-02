bb0dda37feaa7083667388825462399f4a01d04a
Luke Olali <mugapedia@gmail.com>
2020-02-02 16:59:41 +0300

    Declares Firebase wrapper class that initalizes the firebase instance

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,3 +1,5 @@
+import firebase, { app } from 'firebase/app';
+
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -8,3 +10,11 @@ const firebaseConfig = {
   appId: process.env.APP_ID,
   measurementId: process.env.MEASUREMENT_ID
 };
+
+class Firebase {
+  constructor() {
+    app.initializeApp(firebaseConfig);
+  }
+}
+
+export default Firebase;
