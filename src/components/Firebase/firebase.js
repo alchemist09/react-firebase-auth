d2ceebb0eec27332a26d9a0333834a5405ff420b
Luke Olali <mugapedia@gmail.com>
2020-02-02 16:53:45 +0300

    Replace the firebase config object values with environment placeholders from .env.development.local file

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyBOtiep_CZBGioCJ1iLR2cEp5kLiBCuslM",
-  authDomain: "react-firebase-auth-e70ee.firebaseapp.com",
-  databaseURL: "https://react-firebase-auth-e70ee.firebaseio.com",
-  projectId: "react-firebase-auth-e70ee",
-  storageBucket: "react-firebase-auth-e70ee.appspot.com",
-  messagingSenderId: "678187797349",
-  appId: "1:678187797349:web:29349de4fe7daaadb8ef8c",
-  measurementId: "G-R02VMZEDQF"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
 };
