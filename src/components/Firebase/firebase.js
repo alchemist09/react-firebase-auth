d8d63534426ca685e6fe1365f672a88ea89b0a4e
Luke Olali <mugapedia@gmail.com>
2020-02-02 16:43:43 +0300

    Adds firebase project settings

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBOtiep_CZBGioCJ1iLR2cEp5kLiBCuslM",
+  authDomain: "react-firebase-auth-e70ee.firebaseapp.com",
+  databaseURL: "https://react-firebase-auth-e70ee.firebaseio.com",
+  projectId: "react-firebase-auth-e70ee",
+  storageBucket: "react-firebase-auth-e70ee.appspot.com",
+  messagingSenderId: "678187797349",
+  appId: "1:678187797349:web:29349de4fe7daaadb8ef8c",
+  measurementId: "G-R02VMZEDQF"
+};
