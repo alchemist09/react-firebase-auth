e2d98385760b47212b4a09dc878724ef8d4958a8
Luke Olali <mugapedia@gmail.com>
2020-02-07 15:20:46 +0300

    Registers a handler that responds to change events on the form input fields

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -13,13 +13,36 @@ class SignInFormBase extends Component {
     this.state = {
       ...INITIAL_STATE
     }
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(evt) {
+    console.log("handleInputChange() called......");
+    console.log(evt.target.name, ': ', evt.target.value);
+    this.setState({
+      [evt.target.name]: evt.target.value
+    })
   }
 
   render() {
+    const { email, password } = this.state;
     return (
       <form>
-        <input type="text" placeholder="Email address" />
-        <input type="password" placeholder="Password" />
+        <input
+          name="email"
+          type="text" 
+          placeholder="Email Address" 
+          onChange={this.handleInputChange} 
+          value={email} 
+        />
+        <input 
+          name="password"
+          type="password" 
+          placeholder="Password" 
+          onChange={this.handleInputChange} 
+          value={password} 
+        />
         <button type="submit">Sign In</button>
       </form>
     )
