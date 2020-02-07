b116f1950e47e7bdcd2e1fdbedcbc4d552296fbb
Luke Olali <mugapedia@gmail.com>
2020-02-07 14:51:24 +0300

    Declares the SignInFormBase component that used to display a form on the signin page

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SignIn = () => (
+const  INITIAL_STATE = {
+  email: '',
+  password: '',
+  error: null
+}
+
+class SignInFormBase extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      ...INITIAL_STATE
+    }
+  }
+
+  render() {
+    return (
+      <form>
+        <input type="text" placeholder="Email address" />
+        <input type="password" placeholder="Password" />
+        <button type="submit">Sign In</button>
+      </form>
+    )
+  }
+}
+
+const SignInPapge = () => (
   <div>
     <h1>Sign In</h1>
+    <SignInFormBase />
   </div>
 );
 
-export default SignIn;
+export default SignInPapge;
