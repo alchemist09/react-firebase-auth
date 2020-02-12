dca6fe8cf66173ef4e1c7a3f09ce60e23f4bb8e8
Luke Olali <mugapedia@gmail.com>
2020-02-12 14:34:13 +0300

    Refactor the SignOutButton component to enable redirect to signin page upon signout

--- src/components/SignOut/index.js
+++ src/components/SignOut/index.js
@@ -1,8 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { withFirebase } from '../Firebase';
+import { withRouter } from 'react-router-dom';
+import { compose } from 'recompose';
+import * as ROUTES from '../../constants/routes';
 
-const SignOutButton  = ({ firebase }) => (
-  <button onClick={firebase.doSignOut}>Sign Out</button>
-);
+const INITIAL_STATE = {
+  error: ''
+}
 
-export default withFirebase(SignOutButton);
+class SignOutButtonBase extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      ...INITIAL_STATE
+    }
+
+    this.handleSignOut = this.handleSignOut.bind(this);
+  }
+
+  handleSignOut() {
+    this.props.firebase.doSignOut()
+      .then(this.props.history.push(ROUTES.SIGN_IN))
+      .catch(error => {
+        this.setState({ error });
+      })
+  }
+
+  render() {
+    const { error } = this.state;
+    return (
+      <React.Fragment>
+        <button onClick={this.handleSignOut}>Sign Out</button>
+        { error && <p>{error.message}</p>}
+      </React.Fragment>
+    );
+  }
+}
+
+const SignOutButton = compose(
+  withFirebase,
+  withRouter
+)(SignOutButtonBase);
+
+export default SignOutButton;
