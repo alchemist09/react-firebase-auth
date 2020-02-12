bcd914224fb85e88c55312dceb997e2102e13ecd
Luke Olali <mugapedia@gmail.com>
2020-02-12 16:05:39 +0300

    Declares Navigation component to be rendered for authenticated users

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -9,23 +9,29 @@ const Navigation = () => (
       <li>
         <Link to={ROUTES.SIGN_IN}>Sign In</Link> 
       </li>
-      <li>
-        <Link to={ROUTES.LANDING}>Landing</Link> 
-      </li>
-      <li>
-        <Link to={ROUTES.HOME}>Home</Link> 
-      </li>
-      <li>
-        <Link to={ROUTES.ACCOUNT}>Account</Link> 
-      </li>
-      <li>
-        <Link to={ROUTES.ADMIN}>Admin</Link> 
-      </li>
+      
       <li>
         <SignOutButton />
       </li>
     </ul>
   </div>
 );
+
+const NavigationAuth = () => (
+  <ul>
+    <li>
+      <Link to={ROUTES.LANDING}>Landing</Link> 
+    </li>
+    <li>
+      <Link to={ROUTES.HOME}>Home</Link> 
+    </li>
+    <li>
+      <Link to={ROUTES.ACCOUNT}>Account</Link> 
+    </li>
+    <li>
+      <Link to={ROUTES.ADMIN}>Admin</Link> 
+    </li>
+  </ul>
+)
 
 export default Navigation;
