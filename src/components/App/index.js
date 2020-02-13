0f1a695f8a2f032616c3109350745ffc8f3a6a61
Luke Olali <mugapedia@gmail.com>
2020-02-13 14:12:18 +0300

    Removes code used previously for session handling. Reverts the component back to a functional component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,6 +1,4 @@
-import React, { Component } from 'react';
-import { withFirebase } from '../Firebase';
-import { AuthUserContext } from '../Session';
+import React from 'react';
 import { 
   BrowserRouter as Router,
   Route
@@ -18,44 +16,19 @@ import AccountPage from '../Account';
 import * as ROUTES from '../../constants/routes';
 
 
-class App extends Component {
+const App = () => (
+  <Router>
+    <Navigation />
+    <hr />
 
-  constructor(props) {
-    super(props);
+    <Route exact path={ROUTES.LANDING} component={LandingPage} />
+    <Route path={ROUTES.HOME} component={HomePage} />
+    <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+    <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+    <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+    <Route path={ROUTES.ADMIN} component={AdminPage} />
+    <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+  </Router>
+);
 
-    this.state = {
-      authUser: null
-    };
-  }
-
-  componentDidMount() {
-    this.listener = this.props.firebase.auth.onAuthStateChanged(authUser => {
-      authUser ? this.setState({ authUser }) : this.setState({ authUser: null });
-    });
-  }
-
-  componentWillUnmount() {
-    this.listener();
-  }
-
-  render() {
-    return(
-      <AuthUserContext.Provider value={this.state.authUser}>
-        <Router>
-          <Navigation />
-          <hr />
-
-          <Route exact path={ROUTES.LANDING} component={LandingPage} />
-          <Route path={ROUTES.HOME} component={HomePage} />
-          <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-          <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
-          <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
-          <Route path={ROUTES.ADMIN} component={AdminPage} />
-          <Route path={ROUTES.ACCOUNT} component={AccountPage} />
-        </Router>
-      </AuthUserContext.Provider>
-    );
-  }
-}
-
-export default withFirebase(App);
+export default App;
