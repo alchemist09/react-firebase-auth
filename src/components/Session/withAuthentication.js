50f2235934ac3ad7c7a5414e6d954bdac5d40362
Luke Olali <mugapedia@gmail.com>
2020-02-13 14:09:38 +0300

    Transfers the session handling logic from the App component to withAuthentication HOC

--- src/components/Session/withAuthentication.js
+++ src/components/Session/withAuthentication.js
@@ -1,13 +1,37 @@
 import React from 'react';
+import { withFirebase } from '../Firebase';
+import AuthUserContext from './context';
 
 const withAuthentication = Component => {
-  return class withAuthentication extends React.Component {
+  class withAuthentication extends React.Component {
+
+    constructor(props) {
+      super(props);
+  
+      this.state = {
+        authUser: null
+      };
+    }
+  
+    componentDidMount() {
+      this.listener = this.props.firebase.auth.onAuthStateChanged(authUser => {
+        authUser ? this.setState({ authUser }) : this.setState({ authUser: null });
+      });
+    }
+  
+    componentWillUnmount() {
+      this.listener();
+    }
+
     render() {
       return (
-        <Component {...this.props} />
-      )
+        <AuthUserContext.Provider value={this.state.authUser}>
+          <Component {...this.props} />
+        </AuthUserContext.Provider>
+      );
     }
   }
+  return withFirebase(withAuthentication);
 }
 
 export default withAuthentication;
