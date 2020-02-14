dda3c428c392e98fa6c0be331cf558fafa334d36
Luke Olali <mugapedia@gmail.com>
2020-02-14 16:25:11 +0300

    Declares the PasswordChangeForm component used to render HTML form for updating user's password

--- src/components/PasswordChange/index.js
+++ src/components/PasswordChange/index.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const PasswordChange = () => (
-  <div>
-    <h1>Password Change</h1>
-  </div>
-);
+class PasswordChangeForm extends Component {
 
-export default PasswordChange;
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      password_one: '',
+      password_two: '',
+      error: null
+    }
+  }
+
+  render() {
+    const { password_one, password_two} = this.state;
+    return (
+      <form>
+        <input 
+          type="password"
+          name="password_one"
+          value={password_one}
+          placeholder="Enter Password"
+        />
+        <input
+          type="password"
+          name="password_two"
+          value={password_two}
+          placeholder="Confirm Password"
+        />
+        <button type="submit">Change Password</button>
+      </form>
+    );
+  }
+}
+
+export default PasswordChangeForm;
