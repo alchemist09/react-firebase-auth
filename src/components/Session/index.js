39f0b221dfe87511709ea99bce7704c7b5d70480
Luke Olali <mugapedia@gmail.com>
2020-02-17 14:01:21 +0300

    Adds a reference to the withAuthorization HOC to the entry point of the Session module

--- src/components/Session/index.js
+++ src/components/Session/index.js
@@ -1,4 +1,9 @@
 import AuthUserContext from './context';
 import withAuthentication from './withAuthentication';
+import withAuthorization from './withAuthorization';
 
-export { AuthUserContext, withAuthentication }
+export { 
+  AuthUserContext, 
+  withAuthentication,
+  withAuthorization
+}
