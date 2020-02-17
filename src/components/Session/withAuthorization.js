89c637d89c8ffb924395c6bed88c8f653e8a999c
Luke Olali <mugapedia@gmail.com>
2020-02-17 13:42:34 +0300

    Uses the React qualified Component declaration to avoid conflicts with the parameter name passed to the HOC

--- src/components/Session/withAuthorization.js
+++ src/components/Session/withAuthorization.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 const withAuthorization = () => Component => {
-  class WithAuthorization extends Component {
+  class WithAuthorization extends React.Component {
     render() {
       return (
         <Component {...this.props} />
