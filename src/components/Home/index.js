58c633f3913982562a647900c31330a02b5e9985
Luke Olali <mugapedia@gmail.com>
2020-02-17 15:57:22 +0300

    Makes the Home page accessible only to authenticated users by use of the withAuthorization HOC

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { withAuthorization } from '../Session';
 
 const Home = () => (
   <div>
     <h1>Home</h1>
+    <p>Home page is only accessible to authenticated users</p>
   </div>
 );
 
 const condition = authUser => authUser !== null
 
-export default Home;
+export default withAuthorization(condition)(Home);
